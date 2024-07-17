122632af61e5cb069a377fb81cbf5a7d32b27b8f
SkyBlob12 <clement.andrieu12850@gmail.com>
2024-07-17 09:52:44 +0200

    retirer cors à verifier

--- server/index.js
+++ server/index.js
@@ -11,7 +11,9 @@ const authRoutes = require('./routers/auth');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors({
+//     origin: 'http://localhost:3000'
+// }));
 
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
